483ead3f5258f549a858e5a8551ef3457ce2fa1d
Cristian Pinto <cdpinto@me.com>
2016-05-23 15:56:54 -0500

    Agregando phantomjs para correr las pruebas

--- static/features/support/env.rb
+++ static/features/support/env.rb
@@ -2,12 +2,16 @@ require "minitest/autorun"
 require 'capybara'
 require 'capybara/dsl'
 require 'capybara/cucumber'
+require 'capybara-screenshot/cucumber'
+require 'capybara/poltergeist'
 
 World(MiniTest::Assertions)
 MiniTest::Spec.new(nil)
 
 Capybara.configure do |config|
   config.run_server = false
-  config.default_driver = :selenium
-  config.app_host = ENV['APP_HOST'] || 'http://web:3000'
+  config.default_driver = :poltergeist
+  config.current_driver = :poltergeist
+  config.javascript_driver = :poltergeist
+  config.app_host = ENV['APP_HOST'] || 'http://web'
 end
