73b0e08fbabbf993657c70d242105399e80f764d
Cristian Pinto <cdpinto@me.com>
2016-05-31 12:06:00 -0500

    Cambia el endpoint a /parada

--- api.py
+++ api.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 CORS(app)
 db = create_db.run()
 
-@app.route('/api/recurso', methods=['POST'])
+@app.route('/api/parada', methods=['POST'])
 def insert_one():
     repo = Repository(db)
     doc = json.loads(request.data.decode("utf-8"))
     new_doc = repo.save(doc)
-    location = '/api/recurso/{}'.format(new_doc['_id'])
+    location = '/api/parada/{}'.format(new_doc['_id'])
     return json.dumps(new_doc), 201, {'location': location}
 
 if __name__ == '__main__':
--- test/functional/test_api.py
+++ test/functional/test_api.py
@@ -10,9 +10,12 @@ class TestApi:
 
     def test_returns_created_resources_location(self):
         some_value = random_alpha(10)
-        data = {'some_key': some_value}
-        response = self.test_app.post('/api/recurso', data=json.dumps(data))
+        response = self.__insert_document(some_value)
         response_body = json.loads(response.data.decode("utf-8"))
         resource_id = response_body['_id']
         assert_equal(response.status_code, 201)
-        assert_true('/api/recurso/{}'.format(response_body['_id']) in response.location)
+        assert_true('/api/parada/{}'.format(response_body['_id']) in response.location)
+
+    def __insert_document(self, value):
+        data = {'some_key': value}
+        return self.test_app.post('/api/parada', data=json.dumps(data))
