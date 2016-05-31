542b760de61ad50d2fde17f94fefd056c0ed8b19
Cristian Pinto <cdpinto@me.com>
2016-05-31 12:11:38 -0500

    Agrega endpoint para obtener los documentos

--- test/functional/test_api.py
+++ test/functional/test_api.py
@@ -1,6 +1,7 @@
 from api import app
 from test.helpers import *
 import json
+from random import randint
 
 
 class TestApi:
@@ -15,6 +16,20 @@ class TestApi:
         resource_id = response_body['_id']
         assert_equal(response.status_code, 201)
         assert_true('/api/parada/{}'.format(response_body['_id']) in response.location)
+
+    def test_returns_all_resources(self):
+        self.__insert_document('some_value1')
+        self.__insert_document('some_value2')
+
+        response = self.test_app.get('/api/parada')
+        response_body = json.loads(response.data.decode("utf-8"))
+        document = response_body[randint(0,1)]
+
+        assert_equal(response.status_code, 200)
+        assert_equal(len(response_body), 2)
+        assert_true('_id' in document)
+        assert_true('_rev' in document)
+        assert_true('some_key' in document)
 
     def __insert_document(self, value):
         data = {'some_key': value}
