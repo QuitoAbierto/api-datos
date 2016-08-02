a35338f0c731023e27b7e81c1ae4781573b99081
Cristian Pinto <cdpinto@me.com>
2016-08-01 20:56:47 -0500

    Agregando reportes de pruebas

--- task/test.py
+++ task/test.py
@@ -5,17 +5,17 @@ from task.helpers import task_message
 @task
 def integration():
     task_message('Integration Tests')
-    run('nosetests test/integration')
+    run('nosetests test/integration --with-xunit --xunit-file=test_reports/integration.xml')
 
 @task
 def functional():
     task_message('Functional Tests')
-    run('nosetests test/functional')
+    run('nosetests test/functional --with-xunit --xunit-file=test_reports/functional.xml')
 
 @task
 def unit():
     task_message('Unit Tests')
-    run('nosetests test/unit')
+    run('nosetests test/unit --with-xunit --xunit-file=test_reports/unit.xml')
 
 @task(default=True, pre=[unit, integration, functional])
 def all():
